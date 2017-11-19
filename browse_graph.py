d287510d6d7a33c446e83f3f91767ac4c091c13e
Romain MICOLON <rmicolon@e3r12p15.42.fr>
2017-11-19 18:29:49 +0100

    added checked list in browse_graph to avoid endless loops

--- browse_graph.py
+++ browse_graph.py
@@ -1,69 +1,74 @@
-def sym_and(x, matrix, results, indexes):
+def sym_and(x, matrix, results, indexes, chkd):
     links = [i for i, y in enumerate(matrix[x]) if y == 1]
     res = None
     if not links:
         return results[x]
     for z in range(len(links)):
-        results[links[z]] = run_through(links[z], matrix, results, indexes)
+        results[links[z]] = run_through(links[z], matrix, results, indexes, chkd)
         if res is not None:
             res = res & results[links[z]]
         else:
             res = results[links[z]]
     return res
 
-def sym_xor(x, matrix, results, indexes):
+def sym_xor(x, matrix, results, indexes, chkd):
     links = [i for i, y in enumerate(matrix[x]) if y == 1]
     res = None
     if not links:
         return results[x]
     for z in range(len(links)):
-        results[links[z]] = run_through(links[z], matrix, results, indexes)
+        results[links[z]] = run_through(links[z], matrix, results, indexes, chkd)
         if res is not None:
             res = res ^ results[links[z]]
         else:
             res = results[links[z]]
     return res
 
-def sym_not(x, matrix, results, indexes):
+def sym_not(x, matrix, results, indexes, chkd):
     links = [i for i, y in enumerate(matrix[x]) if y == 1]
     res = None
     if not links:
         return results[x]
     for z in range(len(links)):
-        results[links[z]] = run_through(links[z], matrix, results, indexes)
+        results[links[z]] = run_through(links[z], matrix, results, indexes, chkd)
         if res is not None:
             res = res | results[links[z]]
         else:
             res = results[links[z]]
     return 0 if res == 1 else 1
 
-def sym_or(x, matrix, results, indexes):
+def sym_or(x, matrix, results, indexes, chkd):
     links = [i for i, y in enumerate(matrix[x]) if y == 1]
     res = None
     if not links:
         return results[x]
     for z in range(len(links)):
-        results[links[z]] = run_through(links[z], matrix, results, indexes)
+        results[links[z]] = run_through(links[z], matrix, results, indexes, chkd)
         if res is not None:
             res = res | results[links[z]]
         else:
             res = results[links[z]]
     return res
 
-def run_through(x, matrix, results, indexes):
-    if results[x]== 1 and indexes[x][0] != '!':
-        return 1
-    if indexes[x][0] == '+':
-        return sym_and(x, matrix, results, indexes)
-    elif indexes[x][0] == '^':
-        return sym_xor(x, matrix, results, indexes)
-    elif indexes[x][0] == '!':
-        return sym_not(x, matrix, results, indexes)
+def run_through(x, matrix, results, indexes, chkd):
+    if x not in chkd:
+        chkd.append(x)
+        if results[x]== 1 and indexes[x][0] != '!':
+            return 1
+        if indexes[x][0] == '+':
+            return sym_and(x, matrix, results, indexes, chkd)
+        elif indexes[x][0] == '^':
+            return sym_xor(x, matrix, results, indexes, chkd)
+        elif indexes[x][0] == '!':
+            return sym_not(x, matrix, results, indexes, chkd)
+        else:
+            return sym_or(x, matrix, results, indexes, chkd)
     else:
-        return sym_or(x, matrix, results, indexes)
+        return results[x]
 
 def browse(matrix, inits, indexes):
     results = inits
+    chkd = []
     for x in range(len(matrix[0])):
-        results[x] = run_through(x, matrix, results, indexes)
+        results[x] = run_through(x, matrix, results, indexes, chkd)
     return results
